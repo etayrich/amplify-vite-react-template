7596fcd841f9def9692c9d088ff7c890aa925a28
Erin Slater <airslater93@yahoo.com>
2025-03-16 09:13:38 -0500

    "updated app apperance and added signout button"

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import type { Schema } from "../amplify/data/resource";
 import { useAuthenticator } from '@aws-amplify/ui-react';
 import { generateClient } from "aws-amplify/data";
 
+
 const client = generateClient<Schema>();
 
 function App() {
@@ -22,11 +23,13 @@ function App() {
     
   function deleteTodo(id: string) {
     client.models.Todo.delete({ id })
+
   }
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>Checklist</h1>
+      <p>Add new items below.</p>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
@@ -35,13 +38,12 @@ function App() {
           key={todo.id}>{todo.content}</li>
         ))}
       </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
+      <p>Click item to remove them.</p> 
+
+      
+      
+
+          
       <button onClick={signOut}>Sign out</button>
     </main>
   );
